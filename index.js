7e3fdfc0bf1e0392b90c107825c82622dfe1eca4
emgrow <egrow2@nd.edu>
2022-02-01 15:29:26 -0500

    basic outline and login and account creation controls added

---
+++ index.js
@@ -0,0 +1,16 @@
+function signup(){
+    var pswd1 = document.getElementById("password1");
+    var pswd2 = document.getElementById("password2");
+
+    if (!(pswd1.value === pswd2.value)) {
+        alert("Passwords do not match.");
+        pswd1.value = null;
+        pswd2.value = null;
+    }
+    
+    console.log("user account created")
+}
+
+function login(){
+    console.log()
+}
